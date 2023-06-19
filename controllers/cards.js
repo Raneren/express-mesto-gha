5e52778da96febbf900c394d2836fd9850a65928
Igor Malinavichus <crazych4tty@gmail.com>
2023-06-19 21:18:27 +0300

    feat: Добавить роуты для редактирования аватара/инфо пользователя, для лайка/дизлайка карточки. Добавить ошибки

--- controllers/cards.js
+++ controllers/cards.js
@@ -1,21 +1,78 @@
 const Card = require('../models/card');
 
+// Получить все карточки
 module.exports.getCards = (req, res) => {
   Card.find({})
     .populate('owner')
     .then((cards) => res.send(cards))
-    .catch((err) => console.log(`При выполнении кода произошла ошибка ${err.name} c текстом ${err.message}`));
+    .catch((err) => res.status(500).send({ message: `На сервере произошла ошибка: ${err.name}` }));
 };
 
+// Создать карточку
 module.exports.createCard = (req, res) => {
   const { name, link } = req.body;
   Card.create({ name, link, owner: req.user })
     .then((card) => res.send(card))
-    .catch((err) => console.log(`При выполнении кода произошла ошибка ${err.name} c текстом ${err.message}`));
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        res.status(400).send({ message: 'Переданы некорректные данные' });
+      } else {
+        res.status(500).send({ message: 'На сервере произошла ошибка' });
+      }
+    });
 };
 
+// Удалить карточку
 module.exports.deleteCard = (req, res) => {
   Card.findByIdAndRemove(req.params.cardId)
-    .then((card) => res.send(card))
-    .catch((err) => console.log(`При выполнении кода произошла ошибка ${err.name} c текстом ${err.message}`));
+    .then((card) => {
+      if (!card) {
+        res.status(404).send({ message: `Карточка c id: ${req.params.cardId} не найдена` });
+        return;
+      }
+      res.send(card);
+    })
+    .catch((err) => {
+      res.status(500).send({ message: `На сервере произошла ошибка: ${err.name}` });
+    });
 };
+
+// Лайк карточки
+module.exports.likeCard = (req, res) => Card.findByIdAndUpdate(
+  req.params.cardId,
+  { $addToSet: { likes: req.user._id } }, // добавить _id в массив, если его там нет
+  { new: true },
+).then((card) => {
+  if (!card) {
+    res.status(404).send({ message: `Карточка c id: ${req.params.cardId} не найдена` });
+    return;
+  }
+  res.send(card);
+})
+  .catch((err) => {
+    if (err.name === 'ValidationError') {
+      res.status(400).send({ message: 'Переданы некорректные данные' });
+    } else {
+      res.status(500).send({ message: 'На сервере произошла ошибка' });
+    }
+  });
+
+// Дизайк карточки
+module.exports.dislikeCard = (req, res) => Card.findByIdAndUpdate(
+  req.params.cardId,
+  { $pull: { likes: req.user._id } }, // убрать _id из массива
+  { new: true },
+).then((card) => {
+  if (!card) {
+    res.status(404).send({ message: `Карточка c id: ${req.params.cardId} не найдена` });
+    return;
+  }
+  res.send(card);
+})
+  .catch((err) => {
+    if (err.name === 'ValidationError') {
+      res.status(400).send({ message: 'Переданы некорректные данные' });
+    } else {
+      res.status(500).send({ message: 'На сервере произошла ошибка' });
+    }
+  });
--- controllers/users.js
+++ controllers/users.js
@@ -1,21 +1,80 @@
 const User = require('../models/user');
 
+// Получить всех пользователей
 module.exports.getUsers = (req, res) => {
   User.find({})
     .then((users) => res.send(users))
-    .catch((err) => console.log(`При выполнении кода произошла ошибка ${err.name} c текстом ${err.message}`));
+    .catch((err) => res.status(500).send({ message: `На сервере произошла ошибка: ${err.name}` }));
 };
 
+// Получить пользователя по id
 module.exports.getUser = (req, res) => {
   User.findById(req.params.userId)
-    .then((user) => res.send(user))
-    .catch((err) => console.log(`При выполнении кода произошла ошибка ${err.name} c текстом ${err.message}`));
+    .then((user) => {
+      if (!user) {
+        res.status(404).send({ message: `Пользователь c id: ${req.params.userId} не найден` });
+        return;
+      }
+      res.send(user);
+    })
+    .catch((err) => {
+      res.status(500).send({ message: `На сервере произошла ошибка: ${err.name}` });
+    });
 };
 
+// Создать пользователя
 module.exports.createUser = (req, res) => {
   const { name, about, avatar } = req.body;
 
   User.create({ name, about, avatar })
     .then((user) => res.send(user))
-    .catch((err) => console.log(`При выполнении кода произошла ошибка ${err.name} c текстом ${err.message}`));
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        res.status(400).send({ message: 'Переданы некорректные данные' });
+      } else {
+        res.status(500).send({ message: 'На сервере произошла ошибка' });
+      }
+    });
+};
+
+// Обновить инфо пользователя
+module.exports.updateUserInfo = (req, res) => {
+  const { name, about } = req.body;
+
+  User.findByIdAndUpdate(req.user._id, { name, about }, { new: true })
+    .then((user) => {
+      if (!user) {
+        res.status(404).send({ message: `Пользователь c id: ${req.user._id} не найден` });
+        return;
+      }
+      res.send(user);
+    })
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        res.status(400).send({ message: 'Переданы некорректные данные' });
+      } else {
+        res.status(500).send({ message: 'На сервере произошла ошибка' });
+      }
+    });
+};
+
+// Обновить аватар пользователя
+module.exports.updateUserAvatar = (req, res) => {
+  const { avatar } = req.body;
+
+  User.findByIdAndUpdate(req.user._id, { avatar }, { new: true })
+    .then((user) => {
+      if (!user) {
+        res.status(404).send({ message: `Пользователь c id: ${req.user._id} не найден` });
+        return;
+      }
+      res.send(user);
+    })
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        res.status(400).send({ message: 'Переданы некорректные данные' });
+      } else {
+        res.status(500).send({ message: 'На сервере произошла ошибка' });
+      }
+    });
 };
