b784bd28b651389fe1e5048adceb5ae71183a9f7
Igor Malinavichus <crazych4tty@gmail.com>
2023-07-03 15:04:40 +0300

    fix: Добавить валидацию поля avatar при регистрации

--- app.js
+++ app.js
@@ -23,6 +23,7 @@ app.post('/signup', celebrate({
   body: Joi.object().keys({
     name: Joi.string().min(2).max(30),
     about: Joi.string().min(2).max(30),
+    avatar: Joi.string().required().pattern(/https?:\/\/[www.]?[\w-]{1,}.[\w-]{1,}.[-._~:/?#[\]@!$&'()*+,;=]]{1,}#?/),
     email: Joi.string().required().email(),
     password: Joi.string().required(),
   }),
