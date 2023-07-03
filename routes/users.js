663cd4cef12d1f954a7699dc7edd2cb88043ae1d
Igor Malinavichus <crazych4tty@gmail.com>
2023-07-03 17:28:16 +0300

    fix: Исправить ошибки после тестов

--- routes/users.js
+++ routes/users.js
@@ -8,7 +8,7 @@ router.get('/users', getUsers);
 router.get('/users/me', getUserInfo);
 router.get('/users/:userId', celebrate({
   params: Joi.object().keys({
-    userId: Joi.string().length(24).hex().required(),
+    userId: Joi.string().required().length(24).hex(),
   }),
 }), getUserById);
 router.patch('/users/me', celebrate({
